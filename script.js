d8bd4d212b1aaadded72f53c4bffbe5977ba192a
EllaLip <mihaelalip@gmail.com>
2022-06-21 16:18:15 +0200

    JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+//alert("Hello, world.");
+//console.log("Hello");
+var surname = prompt('Greetings friend, may I enquire as to your surname?');
+var a = 1;
+
+for (var i = 1; i<3; i++){
+    console.log(surname);
+    console.log(i);
+    alert("Press okay to continue");
+}
